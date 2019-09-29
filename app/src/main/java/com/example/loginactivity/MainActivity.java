2bce9a73155b98dd2c8f540c57ed4df16b94c324
avi27mitts <avi27mitts@gmail.com>
2019-09-29 18:39:21 +0530

    salary page added buttons on LoginActivity connected

--- app/src/main/java/com/example/loginactivity/MainActivity.java
+++ app/src/main/java/com/example/loginactivity/MainActivity.java
@@ -8,16 +8,26 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    Button login;
+    Button btn_customer_login , btn_worker_login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        login=(Button)findViewById(R.id.login3);
-        login.setOnClickListener(new View.OnClickListener() {
+        btn_customer_login=(Button)findViewById(R.id.btn_customer_login);
+        btn_worker_login=(Button)findViewById(R.id.btn_worker_login);
+
+        btn_customer_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent=new Intent(getApplicationContext(),LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btn_worker_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent( getApplicationContext() , Salary_page.class );
                 startActivity(intent);
             }
         });
