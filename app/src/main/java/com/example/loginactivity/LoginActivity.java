c74b36df97c9e79ef0cb7684f113421dcb7f0f07
jainmohit438 <jainmohit438@gmail.com>
2019-09-30 21:37:07 +0530

    login wala

--- app/src/main/java/com/example/loginactivity/LoginActivity.java
+++ app/src/main/java/com/example/loginactivity/LoginActivity.java
@@ -5,26 +5,37 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
-public class LoginActivity extends Activity {
+public class LoginActivity extends Activity implements View.OnClickListener {
+
+    private Button btnLogin;
+    private Button btnRegister;
+    private EditText tvemail;
+    private EditText tvpassword;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         // setting default screen to login.xml
         setContentView(R.layout.activity_login);
-
-        Button registerScreen = findViewById(R.id.btnRegister);
-
-        // Listening to register new account link
-        registerScreen.setOnClickListener(new View.OnClickListener() {
-
-            public void onClick(View v) {
-                // Switching to Register screen
-                Intent i = new Intent( LoginActivity.this , RegisterActivity.class) ;
-                startActivity(i);
-            }
-        });
+        btnLogin=(Button) findViewById(R.id.btnLogin);
+        tvemail=(EditText) findViewById(R.id.tvemail);
+        tvpassword=(EditText) findViewById(R.id.tvpassword);
+        btnRegister=(Button) findViewById(R.id.btnRegister);
+        btnLogin.setOnClickListener(this);
+        btnRegister.setOnClickListener(this);
 
     }
+
+    @Override
+    public void onClick(View view){
+        if(view==btnLogin){
+            registerUser()
+        }
+        
+    }
+
+
 }
